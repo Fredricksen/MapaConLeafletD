c4a6060590c60d7294cc69ad55f4d5b8f34608b6
Fredricksen <61914188+Fredricksen@users.noreply.github.com>
2020-06-02 17:19:14 -0500

    MuestraMapa

---
+++ js/app.js
@@ -0,0 +1,7 @@
+let map = L.map('map').setView([20.547165, -104.045974], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
