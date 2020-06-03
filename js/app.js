7b9e758e20b15cba1978de71e84753793c599369
Fredricksen <61914188+Fredricksen@users.noreply.github.com>
2020-06-02 22:14:52 -0500

    Marcadoresypopup

--- js/app.js
+++ js/app.js
@@ -1,7 +1,16 @@
-let map = L.map('map').setView([20.547165, -104.045974], 13);
+let map = L.map('map').setView([20.547165, -104.045974], 9);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   attribution:
     '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+let marker1 = L.marker([20.547165, -104.045974]).addTo(map);
+let marker2 = L.marker([19.547165, -104.045974])
+   .bindPopup('<h3>Titul0</h3>Hola -estoy aqui-') //agregar marxador con un popup 
+   .addTo(map)
+   .openPopup();
+
+let popup = L.popup().setLatLng([20.847165, -104.145974]).setContent('Este es el popup'); //poup independiente
+
+marker1.bindPopup(popup).openPopup(); //AGREGAR (asignar) POPUP AL MARKADOR
